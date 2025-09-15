351c21cfc1d795021b40be21127b607137bd5c43
Cakaras <bangamin2353@gmail.com>
2025-09-16 05:56:26 +0800

    pertemuan 5

---
+++ main.js
@@ -0,0 +1,12 @@
+// console.log("Hello, World!");
+
+// function greet(name) {
+//     return `Hello, ${name}!`;
+
+// console.log(greet("Alice"));
+// }
+
+// console.log(greet("Bob"));
+
+const mainText = document.getElementById("main-text");
+mainText.innerHTML = "Hello, World!";
