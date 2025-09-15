00e490657f5a63996faa9bbe1002c81b81e05f60
Cakaras <bangamin2353@gmail.com>
2025-09-16 06:04:52 +0800

    pertemuan 5

--- main.js
+++ main.js
@@ -9,4 +9,16 @@
 // console.log(greet("Bob"));
 
 const mainText = document.getElementById("main-text");
-mainText.innerHTML = "Hello, World!";
+// mainText.innerHTML = "Hello, World!";
+
+class mahasiswa {
+    constructor(nama, nim, jurusan) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+    }
+}
+
+const mhs = new mahasiswa("egan", "123", "Informatika");
+mainText.innerText = mhs.nama;
+mainText.innnerText = mhs.nama;
